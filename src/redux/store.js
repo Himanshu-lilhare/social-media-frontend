9d07380fdcc41e0fb9e62f0fe0310a3a076d1131
Raj <rajlilhare200@gmail.com>
2023-04-01 04:46:22 -0700

    add link

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,9 @@ import { fetchingAndSendingMessages, getChatsOrCreateChats } from "./reducers/ch
 import { likePostReducer, myPostsReducer, othersPostsReducer, postReducer } from "./reducers/postReducer";
 import { allUserReducer, getOtherUser, userReducer } from "./reducers/userReducer";
 
-export const serverLink = "http://localhost:5000";
-// export const serverLink = "https://social-media-backend-mu.vercel.app";
-// hfdfu
+// export const serverLink = "http://localhost:5000";
+export const serverLink = "https://social-media-backend-mu.vercel.app";
+
 export const store=configureStore({
     reducer:{
         userReducer:userReducer,
